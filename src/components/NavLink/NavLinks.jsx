63826cf8684f5c011c029ff297cbbc20511f9703
illya-onyshchuk <llayonys@gmail.com>
2023-10-30 20:09:32 +0200

    code refactor

--- src/components/NavLink/NavLinks.jsx
+++ src/components/NavLink/NavLinks.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { tabs } from '../../testdb/tabs';
 import cl from './NavLinks.module.css'
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const NavLinks = () => {
 
-  const setActive = ({isActive}) => isActive ? cl.active__link : '';
-
   return (
     <Navbar expand='lg' bg="light" data-bs-theme="light">
       <Container>
-        <Nav className={cl.navbar__costom}>
+        <Nav className={cl.navbar}>
           {[...tabs]
           .sort((a,b) => a.order - b.order)
           .map(tab => (
              <NavLink
                key={tab.id}
                to={`/${tab.id}`}
-               className={setActive}
+               className={({isActive}) =>  isActive ? cl.active__link : ''}
               >
               {tab.title}
              </NavLink>
